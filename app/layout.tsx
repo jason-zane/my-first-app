98ddd8e989cae370a4e172017b07a18698e725bf
jason-zane <jasonzanehunt@gmail.com>
2026-02-27 09:32:48 +1100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Toaster } from 'sonner'
 import './globals.css'
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
         <SpeedInsights />
         <Toaster position="bottom-right" richColors closeButton />
       </body>
